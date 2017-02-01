a7acf20214684282d4f4f368df63eca038041168
sethlam <sethlam1206@gmail.com>
2017-01-31 20:19:57 -0700

    merge

--- app/src/main/java/com/example/brian/androidchess/MainActivity.java
+++ app/src/main/java/com/example/brian/androidchess/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.brian.androidchess;
 
-import android.content.Context;
-import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Switch;
